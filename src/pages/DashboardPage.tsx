c6cbe6752579e9d718f77fb3e009ebd0267ed903
Stas Arsoba <s@onestar.dev>
2022-10-26 10:31:15 +0200

    Added mock data to the dashboard

--- src/pages/DashboardPage.tsx
+++ src/pages/DashboardPage.tsx
@@ -5,29 +5,26 @@ import { Page } from '~/features/core'
 import styles from './DashboardPage.module.scss'
 
 type MyRowItem = {
-  name: string
-  value: string
   id: number
+  name: string
+  validators: string
+  hosting: string
+  performance: string
 }
 
 const tableProps: TableProps<MyRowItem> = {
   columns: [
     { title: 'ID', render: ({ id }) => <Typography>{id}</Typography> },
     { title: 'NAME', render: ({ name }) => <Typography>{name}</Typography> },
-    { title: 'VALUE', render: ({ value }) => <Typography>{value}</Typography> },
-    {
-      title: ['NAME /', 'VALUE'],
-      render: ({ name, value }) => (
-        <Typography>
-          {name} / {value}
-        </Typography>
-      )
-    }
+    { title: 'VALIDATORS', render: ({ validators }) => <Typography>{validators}</Typography> },
+    { title: 'HOSTING', render: ({ hosting }) => <Typography>{hosting}</Typography> },
+    { title: 'PERFORMANCE', render: ({ performance }) => <Typography>{performance}</Typography> }
   ],
   rows: [
-    { name: 'Hello 1', value: 'World 1', id: 1 },
-    { name: 'Hello 2', value: 'World 2', id: 2 },
-    { name: 'Hello 3', value: 'World 3', id: 3 }
+    { id: 11, name: 'Allnodes', validators: '3', hosting: 'AWS', performance: '99.2%' },
+    { id: 23, name: 'Onestar', validators: '3', hosting: 'GCP', performance: '98.2%' },
+    { id: 30, name: 'SafeStake', validators: '3', hosting: 'Azure', performance: '99.4%' },
+    { id: 400, name: 'BloxStaking', validators: '3', hosting: 'AWS', performance: '99.1%' }
   ],
   // eslint-disable-next-line no-console
   onRowClick: (rowIndex) => console.log(`rowIndex: ${rowIndex}`)
@@ -37,12 +34,9 @@ export function DashboardPage(): JSX.Element {
   return (
     <Page className={styles.DashboardPage} title="Dashboard">
       <div className={styles.Info}>
-        <InfoCard className={styles.InfoCard} title="Some Info 1" info="0.00%" variant="large" />
-        <InfoCard className={styles.InfoCard} title="Some Info 2" info="0.00%" variant="large" />
-        <InfoCard className={styles.InfoCard} title="Some Info 3" info="$0.00" variant="large" />
-        <InfoCard className={styles.InfoCard} title="Some Info 4" info="0.01" variant="large" />
-        <InfoCard className={styles.InfoCard} title="Some Info 5" info="0.02" variant="large" />
-        <InfoCard className={styles.InfoCard} title="Some Info 6" info="0.03" variant="large" />
+        <InfoCard className={styles.InfoCard} title="TVL" info="$30,21.69" variant="large" />
+        <InfoCard className={styles.InfoCard} title="$321.69" info="3" variant="large" />
+        <InfoCard className={styles.InfoCard} title="Operators in the Pool" info="4" variant="large" />
       </div>
       <Table className={styles.Table} {...tableProps} />
     </Page>

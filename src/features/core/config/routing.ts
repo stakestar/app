caa6abb61633901f727e1b8685e53ee3c14fa094
Stas Arsoba <s@onestar.dev>
2022-12-01 09:56:04 +0100

    Set staking as main page

--- src/features/core/config/routing.ts
+++ src/features/core/config/routing.ts
@@ -1,13 +1,13 @@
 import { DashboardPage, StakingPage } from '~/pages'
 
 export const routes = [
-  { path: '/', Component: DashboardPage },
-  { path: '/staking', Component: StakingPage },
+  { path: '/', Component: StakingPage },
+  { path: '/dashboard', Component: DashboardPage },
   { path: '/docs', Component: StakingPage }
 ] as const
 
 export const menu = [
-  { title: 'Dashboard', path: '/', soon: false },
-  { title: 'Staking', path: '/staking', soon: false },
+  { title: 'Staking', path: '/', soon: false },
+  { title: 'Dashboard', path: '/dashboard', soon: false },
   { title: 'Docs', path: '/docs', soon: true }
 ]

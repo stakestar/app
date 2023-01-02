47dcc1b2d3b6f90b428668ed7d9a641b220c7812
Stas Arsoba <s@onestar.dev>
2023-01-02 13:10:41 +0100

    Lint

--- src/features/staking/components/Faq.tsx
+++ src/features/staking/components/Faq.tsx
@@ -4,23 +4,27 @@ import { Collapse } from './Collapse'
 import styles from './Faq.module.scss'
 
 const faq = [
-  { 
-    title: 'What is the StakeStar application?', 
+  {
+    title: 'What is the StakeStar application?',
     description: `StakeStar is a decentralized staking application built on top of the SSV network. 
-      StakeStar allows users to pool and stake their Eth in a secure way.` },
-  { title: 'How is StakeStar different from other staking pools?', 
-    description: 
-    `The StakeStar application is built on top of the SSV network which has developed Distributed 
+      StakeStar allows users to pool and stake their Eth in a secure way.`
+  },
+  {
+    title: 'How is StakeStar different from other staking pools?',
+    description: `The StakeStar application is built on top of the SSV network which has developed Distributed 
       Validator Technology (DVT). DVT enables trustless staking through multi-operator validation. 
-      In simple words, DVT ensures that no single party has access to your funds through the validator's private keys.` },
-  { title: 'How does StakeStar work?', 
+      In simple words, DVT ensures that no single party has access to your funds through the validator's private keys.`
+  },
+  {
+    title: 'How does StakeStar work?',
     description: `
     The StakeStar application works similarly to other staking pool solutions. Users can stake their 
     Eth through the Staking page and will get a receipt token ssETH in return. Later, after the Ethereum 
     network enables withdrawals, users will be able to unstake their Eth alongside with accumulated rewards.
     <br /><br />
     It is worth noting that StakeStar doesn't aim to be a liquid staking solution. That means StakeStar 
-    doesn't maintain liquidity pools of ssETH/ETH assets on DEXes.`}
+    doesn't maintain liquidity pools of ssETH/ETH assets on DEXes.`
+  }
 ]
 
 export function Faq(): JSX.Element {

a8cb3a3901bcbb011a06c616c25c8f940fcbf247
Roman Romanovsky <roman.romanovsky.github@gmail.com>
2023-03-09 16:30:10 +0100

    Add website link

--- src/features/core/components/App/Sidebar/Sidebar.tsx
+++ src/features/core/components/App/Sidebar/Sidebar.tsx
@@ -50,9 +50,19 @@ export function Sidebar(): JSX.Element {
             </Link>
           </li>
           <li className={styles.LinksRow}>
-            <Icon icon="telegram" fillColor={theme.text} withHover />
-            <Icon icon="twitter" fillColor={theme.text} withHover />
-            <Icon icon="medium" fillColor={theme.text} withHover />
+            {/*<Icon icon="telegram" fillColor={theme.text} withHover />*/}
+            <a href="https://twitter.com/stakestar_pool" target="_blank" rel="noopener noreferrer">
+              <Icon icon="twitter" fillColor={theme.text} withHover />
+            </a>
+            {/*<Icon icon="medium" fillColor={theme.text} withHover />*/}
+            <a className={styles.Icon} href="https://stakestar.io" target="_blank" rel="noopener noreferrer">
+              <svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" viewBox="0 0 512 512">
+                <path d="M256 48C141.13 48 48 141.13 48 256s93.13 208 208 208 208-93.13 208-208S370.87 48 256 48Z" />
+                <path d="M256 48c-58.07 0-112.67 93.13-112.67 208S197.93 464 256 464s112.67-93.13 112.67-208S314.07 48 256 48Z" />
+                <path d="M117.33 117.33c38.24 27.15 86.38 43.34 138.67 43.34s100.43-16.19 138.67-43.34M394.67 394.67c-38.24-27.15-86.38-43.34-138.67-43.34s-100.43 16.19-138.67 43.34" />
+                <path d="M256 48v416M464 256H48" />
+              </svg>
+            </a>
           </li>
         </ul>
         <Switch

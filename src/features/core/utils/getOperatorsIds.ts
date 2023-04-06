79e45fefa75787310a7115c0840bcf8e5b0e80c2
Roman Romanovsky <roman.romanovsky.github@gmail.com>
2023-04-06 14:36:01 +0200

    Bump @stakestar/contracts to 0.17.0

--- src/features/core/utils/getOperatorsIds.ts
+++ src/features/core/utils/getOperatorsIds.ts
@@ -2,6 +2,6 @@ import { OPERATOR_IDS } from '@stakestar/contracts'
 
 import { getChainNameId } from './getChainNameId'
 
-export function getOperatorsIds(): number[] {
-  return OPERATOR_IDS[getChainNameId()]
+export function getOperatorsIds(): string[] {
+  return OPERATOR_IDS[getChainNameId()].map((operatorId) => operatorId.toString())
 }

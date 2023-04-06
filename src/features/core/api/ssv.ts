65559d347adf1186465d3575127f3dcfc8ab76e2
Stas Arsoba <s@onestar.dev>
2023-04-06 15:43:36 +0200

    Update ssv api url

--- src/features/core/api/ssv.ts
+++ src/features/core/api/ssv.ts
@@ -2,6 +2,6 @@ import axios from 'axios'
 
 const ssvClient = axios.create()
 
-ssvClient.defaults.baseURL = 'https://api.ssv.network/api/v2/prater'
+ssvClient.defaults.baseURL = 'https://api.ssv.network/api/v3/prater'
 
 export { ssvClient }

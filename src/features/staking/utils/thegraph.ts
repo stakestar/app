221aadc70db5e3ca3936a074d8c5f7a68087ab6e
Stas Arsoba <s@onestar.dev>
2023-05-05 10:30:44 +0200

    Calculate apr based on 7 days

--- src/features/staking/utils/thegraph.ts
+++ src/features/staking/utils/thegraph.ts
@@ -2,12 +2,12 @@ import { TokenRateDaily } from '@stakestar/subgraph-client/dist/types.d.ts'
 import BigNumber from 'bignumber.js'
 
 export function calculateApr(tokenRateDailies: Array<Pick<TokenRateDaily, 'id' | 'date' | 'rate'>>): number {
-  if (tokenRateDailies.length < 3) {
+  if (tokenRateDailies.length < 7) {
     return 0
   }
 
   const firstState = tokenRateDailies[0]
-  const lastState = tokenRateDailies[2]
+  const lastState = tokenRateDailies[6]
 
   const firstRate = new BigNumber(String(firstState.rate))
   const secondRate = new BigNumber(String(lastState.rate))

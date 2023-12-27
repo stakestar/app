14b51c15c9d7215e58d3e98190015003249e2a6d
Stas Arsoba <s@onestar.dev>
2023-12-27 14:57:43 +0100

    Mainnet therpah

--- src/features/core/config/urls.ts
+++ src/features/core/config/urls.ts
@@ -1,2 +1,2 @@
-export const thegraphUrl = 'https://subgraph.stakestar.io/subgraphs/name/stakestar-testnet'
+export const thegraphUrl = 'https://subgraph.stakestar.io/subgraphs/name/stakestar'
 export const uniswapThegraphUrl = 'https://api.thegraph.com/subgraphs/name/uniswap/uniswap-v3'
